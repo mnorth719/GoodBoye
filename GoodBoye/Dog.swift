01b56dd8f3cc80f015e4cb376e61320ad3543ef3
Matt North <matt@weareenvoy.com>
2017-04-06 17:50:46 -0700

    Good dogs for all

---
+++ GoodBoye/Dog.swift
@@ -0,0 +1,14 @@
+//
+//  Dog.swift
+//  GoodBoye
+//
+//  Created by Envoy on 4/6/17.
+//  Copyright © 2017 mmn. All rights reserved.
+//
+
+import UIKit
+
+struct Dog {
+    var breed: String
+    var image: UIImage?
+}
--- GoodBoye/FavoriteDogsViewController.swift
+++ GoodBoye/FavoriteDogsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoriteDogsViewController.swift
 //  GoodBoye
 //
 //  Created by Envoy on 4/6/17.
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoriteDogsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
